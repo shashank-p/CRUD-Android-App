0a5ac98492636a16c0de19d5b2dfa3ca932f9a46
Rizky Agung Prayogi <rizkyden06@gmail.com>
2022-08-05 01:58:08 +0700

    Edit Error

--- app/src/main/java/com/shashank/crud/MyAdapter.java
+++ app/src/main/java/com/shashank/crud/MyAdapter.java
@@ -5,13 +5,14 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AlertDialog;
-import android.support.v7.widget.CardView;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
